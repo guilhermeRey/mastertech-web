b54afd7609e1201066892d0f57c53553bc20edee
Guilherme Rey <poli.rey@gmail.com>
2016-01-14 17:51:59 -0200

    aula 4 mastertech

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
 // Hello! We're here to help and serve you. (:
 $(document).ready(function () {
-  
-});;
+  var telefones = [
+    'Motorola X',
+    'iPhone 5S'
+  ];
+
+  var $lista = $('#lista-dinamica');
+
+  for(var i = 0; i < telefones.length; i++) {
+    var nomeDoTelefone = telefones[i];
+    var blocoHtml = '<div class="col-sm-4 borda-vermelha"><b>'
+                        + nomeDoTelefone + '</b></div>';
+
+    $lista.append(blocoHtml);
+  }
+
+});
